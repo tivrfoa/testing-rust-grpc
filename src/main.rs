4d7d1b83c792a6d1dbd2870163ce8f008ed25163
tivrfoa <lescoutinhovr@gmail.com>
2025-02-01 18:35:13 -0300

    Location of Rust source generated by protobuf
    ./target/debug/build/grpc1-9a4822a51ebff50d/out/hello.rs

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use tonic::{transport::Server, Request, Response, Status};
+use hello::greeter_server::{Greeter, GreeterServer};
+use hello::{HelloReply, HelloRequest};
+
+pub mod hello {
+    tonic::include_proto!("hello");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        let name = request.into_inner().name;
+        let reply = hello::HelloReply {
+            message: format!("Hello, {}!", name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    println!("GreeterServer listening on {}", addr);
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
