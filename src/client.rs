b3151f5ec78828b5942c490a648691a2df27a58a
tivrfoa <lescoutinhovr@gmail.com>
2025-02-04 22:38:12 -0300

    Sending grpc with curl!
    curl -v --http2-prior-knowledge localhost:50051/hello.Greeter/SayHello -X POST \
         -H "Content-Type: application/grpc" \
         --data-binary @m4 --output response.out
    Note: Unnecessary use of -X or --request, POST is already inferred.
    * Host localhost:50051 was resolved.
    * IPv6: ::1
    * IPv4: 127.0.0.1
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
      0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0*   Trying [::1]:50051...
    * Connected to localhost (::1) port 50051
    * [HTTP/2] [1] OPENED stream for http://localhost:50051/hello.Greeter/SayHello
    * [HTTP/2] [1] [:method: POST]
    * [HTTP/2] [1] [:scheme: http]
    * [HTTP/2] [1] [:authority: localhost:50051]
    * [HTTP/2] [1] [:path: /hello.Greeter/SayHello]
    * [HTTP/2] [1] [user-agent: curl/8.6.0]
    * [HTTP/2] [1] [accept: */*]
    * [HTTP/2] [1] [content-type: application/grpc]
    * [HTTP/2] [1] [content-length: 12]
    > POST /hello.Greeter/SayHello HTTP/2
    > Host: localhost:50051
    > User-Agent: curl/8.6.0
    > Accept: */*
    > Content-Type: application/grpc
    > Content-Length: 12
    >
    } [12 bytes data]
    < HTTP/2 200
    < content-type: application/grpc
    < date: Wed, 05 Feb 2025 01:36:32 GMT
    <
    { [20 bytes data]
    < grpc-status: 0
    { [0 bytes data]
    100    32    0    20  100    12  10881   6528 --:--:-- --:--:-- --:--:-- 32000
    * Connection #0 to host localhost left intact

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,11 @@
 use hello::greeter_client::GreeterClient;
 use hello::HelloRequest;
 
+use prost::Message;
+
+use std::fs::File;
+use std::io::Write;
+
 pub mod hello {
     tonic::include_proto!("hello");
 }
@@ -9,9 +14,29 @@ pub mod hello {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GreeterClient::connect("http://[::1]:50051").await?;
 
-    let request = tonic::Request::new(HelloRequest {
+    let hello_request = HelloRequest {
         name: "Tonic".into(),
-    });
+    };
+    let encoded_message = hello_request.encode_to_vec();
+    println!("{:02x?}", encoded_message);
+
+    // gRPC requires a 5-byte prefix: 1 byte for compression flag (0 = none), 4 bytes for message length
+    let mut grpc_frame = vec![0u8]; // First byte: compression flag (0 = no compression)
+    grpc_frame.extend_from_slice(&(encoded_message.len() as u32).to_be_bytes()); // 4-byte length
+    grpc_frame.extend_from_slice(&encoded_message); // Append the actual serialized message
+
+    // Print the full message in hex (useful for debugging)
+    println!("gRPC Encoded Message (Hex): {:02X?}", grpc_frame);
+
+    let mut file = File::create("m4")?;
+    file.write_all(&grpc_frame)?;
+
+    // Print in base64 (needed for curl)
+    let base64_message = base64::encode(&grpc_frame);
+    println!("gRPC Encoded Message (Base64): {}", base64_message);
+
+    let request = tonic::Request::new(hello_request);
+
 
     let response = client.say_hello(request).await?;
 
